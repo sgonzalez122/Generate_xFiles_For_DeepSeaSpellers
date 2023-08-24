678663c9622535caab500a6a7cf1f23c66f2cb57
sgonzalez122 <60163234+sgonzalez122@users.noreply.github.com>
2023-08-24 16:19:50 -0700

    Update Generate_xFiles_For_DeepSeaSpellers.m
    Made updates to factor in Practice Trials

--- Generate_xFiles_For_DeepSeaSpellers.m
+++ Generate_xFiles_For_DeepSeaSpellers.m
@@ -1,47 +1,42 @@
-% File: Generate_xFiles_For_DeepSeaSpellers.m
+% File: Generate_xFilesForMagicWords.m
 % Programer: Stephen A. Gonzalez
-% Date Created: 8/23/2023
+% Date Created: 4/28/2023
 % Purpose: 
-%   Creates xFiles used for creating tasks in xDiva
-%   xFiloes are created using either .png images or Matlab bitmaps (.bmp)
+%   This script will generate xFiles, to be used for creating tasks in xDiva
+%   xFiles are created using either .png images or .mat file bitmaps.
 %
 % --------------------------------------------------------------------------- 
 % Required files: 
-%   Folder containing .png  image files               ("stim_images" folder)
-%   Folder containing .bmp  image files               ("stim_images_woreds")
-%   Folder containing .xslx Stimulus Excel Files      ("stim_excel_files" folder) 
+%   Folder containing .png image files          ("images" folder)
+%   Folder containing .xslx condition files     ("conditions" folder) 
 %
 % ---------------------------------------------------------------------------
 % Output(s)
-%   xFile: .mat file to be used in xDiva for task design    ("xFiles" folder)
+%   xFile: .mat file to be used in xDiva for task design                            ("xFiles" folder)
+%   
+%
 
 
+%% Clear Workspace
+tic
+clear
+close all
+clc
+cd ~/Documents/Fang/Magic_Words_Spring_2023/ %Set Working Directory
 
-%% Set Up
-tic %Start Timer 
-
-clear; close all ; clc; % Clear Workspace
-
-% mainPath = ~/Documents/YOUR PATH/create_xFiles_package/
-mainPath = "~/Documents/Lindsey/Deep_Sea_Spellers/create_xFiles_package/";
+mainPath = '~/Documents/Fang/Magic_Words_Spring_2023/';
 xFilesPath = fullfile(mainPath,'xFiles');
-ExcelFolderPath = fullfile(mainPath,'stim_excel_files/');
-imageFolderPath = fullfile(mainPath,'stim_images/');
-wordFolderPath = fullfile(mainPath,'stim_images_words/');
 
-% Change Directory to Main Path
-cd(mainPath)
-
-% Create xFiles Folder
-if (~exist('xFile','dir'))
+% Create main directory for X-Files
+if (~exist('xFiles', 'dir'))
     mkdir(xFilesPath);
 end
+%% Set up user inputs
+classNum = 3; % Number of Class's
 
-%% User Inputs
-classNum = 3;
-conditions = 3;
-trials = 10; 
+conditions = 3; %Range of Conditions
 
+trials = 10; %Number of Trials in each Condition
 
 % Sizing variables (VV)
 objectVerticalSizePix = 80; % vertical size of the words in pixels.
@@ -54,89 +49,101 @@ nCols_full	= 500;
 nRows_small	= 160;
 nCols_small	= 600; % 600;
 
-
-for iClass = 1:classNium
-
-    for iCondition = 1:conditions
-        % Assign Excel File Path
-        ExcelFilePath = [ExcelFolderPath 'FILE_NAME' num2str(iClass) '.xlsx']; % Path to excel files
+for classi = 1:classNum
+    for condition = 1:conditions
+        %Change file paths for your computer
+        excelFile = [pwd filesep 'conditions/MW_CLASS' num2str(classi) '.xlsx'];  %Path to excel files
+        emojiFilePath = [pwd filesep 'emoji_Images/']; %Path to Emoji Images
+        wordFilePath = [pwd filesep 'word_Images/']; %Path to Word Images
+    
+        %% Choosing version and initializing variables
+        conditionData = readtable(excelFile, 'Sheet', condition);  %Read in Excel File
+        conditionData = conditionData(:,2:11);
+        %digitImageFile = matfile(digitImageMatFile);
         
-        % % Read in Condition Data
-        conditionData = readtable(ExcelFilePath, 'Sheet', iCondition); %Read in Condition Table
-        conditionData = conditionData(:,2:11); %Columns 2 through 11, will depend on the Excel lset up
-
-        for iTrial = 1:trials
-            % Convert data to string, split strings at New-Line Characters, and transpose
-            stimData = cellstr(transpse(splitlines(string(conditionData.(trial)))));
+        for trial = 1:trials
+            % Convert data to string, split strings, and transpose like above
+            % Added 1 to the trial iterator to account for the stimNum Column
+            stimData = cellstr(transpose(splitlines(string(conditionData.(trial)))));
+            
             stimImages = [];
+    
+            %% Draft code
+            for imgID = 1:size(stimData, 2)
+                if contains(stimData{:, imgID},'.png') == 1 % .PNG File
+                    emoji_rgb = imread(strcat(emojiFilePath, stimData{:, imgID}));
 
-            for imgId = 1:size(stimData, 2)
-                if contains(stimData{:, imgId}, '.png') == 0 % .bmp File
-                    %%% Read in Word Bit Map %%%
+                    %if classi == 3
+                    %    temp = find(emoji_rgb==1);
+                    %    emoji_rgb(temp) = 0;
+                    %end
 
+                    emoji_rgb = im2double(emoji_rgb); %convert to double
 
-                    % Read in Gray Scale Bit Map
-                    wordImg_gray = imread(strcat(wordFolderPath, stimData{:, imgId}, '.bmp')); 
+                    %Convert black background to grey    
+                    greycol = 0.5010;
+                    [rows, cols, panes] = size(emoji_rgb);
+                    blacks = find( ~sum(emoji_rgb,3) );
+                    emoji_rgb( [blacks, blacks + rows*cols, blacks + 2*rows*cols]) = greycol;
+                    %imshow(emoji_rgb)
 
-                    %Convert Gray Scale Image to Index Image
-                    [wordImg_Ind, wordImg_Cmap] = gray2ind(wordImg_gray,500); 
+                    stimImages(:,:,:, imgID) = emoji_rgb;
+                  
 
-                    % Converty INdex Image to Truecolor RGB Image
-                    word_rgb = ind2rgb(wordImg_Ind, wordImg_Cmap); 
+                elseif contains(stimData{:, imgID},'.png') == 0 % TEXT File
+                    %Read in Word Bit Map
+                    wordImg_gray = imread(strcat(wordFilePath, stimData{:, imgID}, '.bmp')); % Read in Gray Scale bit map
+                    [wordImg_Ind, wordImg_Cmap] = gray2ind(wordImg_gray,500); % Convert Gray Scale Image ==> Index Image
+                    word_rgb = ind2rgb(wordImg_Ind, wordImg_Cmap); % Convert Index Image ==> Truecolor RGB Image
+
+                    stimImages(:,:,:, imgID) = word_rgb; %Assign to structure
+                    
+                    %{
+                    wordIMG = imread(strcat(wordFilePath, stimData{:, imgID}, '.bmp'));
+                    imshow(wordIMG);
+
+                    [wordIMG_Ind, wordImgCmap] = gray2ind(wordIMG,500);
+
+                    imshow(wordIMG_Ind, wordImgCmap);
+
+                     word_Ind2rgb = ind2rgb(wordIMG_Ind,wordImgCmap);
+
+                    word_Gray2RGB = grs2rgb(wordIMG);
+                    imshow(word_Gray2RGB); 
+                    %}
+                
 
                     
-
-                    % Assign to Stim Image Structure
-                    stimImages(:,:,:, imgID) = word_rgb;
-
-
-                elseif contains(stimData{:, imagID}, '.png') == 1 % .png File
-                    %%% Read in .png Image %%%
-                    img_rgb = imread(strcat(imageFolderPath, stimData{:, imgID}));
-
-                    img_rgb = im2double(img_rgb); % convert to double
-
-                    % Convert Black background to grey
-                    greycol = 0.5010;
-                    [rows, cols, panes] = size(img_rgb);
-                    black_values = find( ~sum(img_rgb,3));
-                    img_rgb([black_values, black_values + rows*cols, black_values + 2*rows*cols]) = greycol;
-                    %imshow(img_rgb) 
-
-                    stimImages(:,:,:, imgId) = img_rgb;
-
-
+                    %Genereate Text .mat File
+                    %image = makeTextImages(stimData{:, imgID}, nRows_small, nCols_small);  
                 end
             end
-
-
-            %% Create sub-directories for xFiles
-            
-            % Create Class Sub-Folder
-            classFolder = strcat(xFilesPath, '/Class_', (num2str(iClass)));
+                
+    
+            %% Saving the output files
+            images = single(stimImages);
+            strings = stimData;
+            %objectVerticalSizePix = ;
+           
+            % Create sub-directories for X-Files
+            classFolder = strcat(xFilesPath, '/Class_', (num2str(classi)));
             if (~exist((classFolder),'dir'))
                 mkdir(classFolder);
             end
 
-            % Create Condition Sub-Folder
-            conditionFodler = strcat(classFolder, '/Class_', num2str(iClass), '_Condition_', num2str(iCondition));
+            conditionFolder = strcat(classFolder, '/Class_', num2str(classi), '_Condition_', num2str(condition)+1);
             if (~exist((conditionFolder),'dir'))
                 mkdir(conditionFolder);
             end
 
-            %% Save Out
-            images = single(stimImages);
-            strings = stimData; 
-            %objectVerticalSizePix = ;
-
-            save([xFilesPath filesep 'Class_' num2str(iClass) ...
-                '/Class_' num2str(iClass) '_Condition_' num2str(iCondition) ... 
-                '/DSS_Class_' num2str(iClass) '_condition_' num2str(iCondition) '_Trial_' num2str(iTrial) '.mat'], ...
-                'images', 'strings'); %, 'objectVerticalSizePiox');
-
-
+            save([pwd filesep 'xFiles/Class_'  num2str(classi) ...
+                '/Class_' num2str(classi) '_Condition_' (num2str(condition)+1) ...
+                '/MW_Class_' num2str(classi) '_condition_' (num2str(condition)+1) '_Trial_' num2str(trial) '.mat'], ...
+                'images', 'strings'); %, 'objectVerticalSizePix');
+         
+            
         end
     end
 end
 
-toc % End Timer
+toc
