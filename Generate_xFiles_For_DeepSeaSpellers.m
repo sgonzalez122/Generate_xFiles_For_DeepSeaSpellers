bae90b6cd955095857afab3eb1a21e3b55e0ab62
sgonzalez122 <60163234+sgonzalez122@users.noreply.github.com>
2023-08-25 16:44:03 -0700

    Update Generate_xFiles_For_DeepSeaSpellers.m
    Added Code to handle Audio Stimulus Data

--- Generate_xFiles_For_DeepSeaSpellers.m
+++ Generate_xFiles_For_DeepSeaSpellers.m
@@ -1,42 +1,54 @@
-% File: Generate_xFilesForMagicWords.m
+% File: Generate_xFiles_For_DeepSeaSpellers.m
 % Programer: Stephen A. Gonzalez
-% Date Created: 4/28/2023
+% Date Created: 8/23/2023
 % Purpose: 
-%   This script will generate xFiles, to be used for creating tasks in xDiva
-%   xFiles are created using either .png images or .mat file bitmaps.
+%   Creates xFiles used for creating tasks in xDiva
+%   xFiloes are created using either .png images or Matlab bitmaps (.bmp)
 %
 % --------------------------------------------------------------------------- 
 % Required files: 
-%   Folder containing .png image files          ("images" folder)
-%   Folder containing .xslx condition files     ("conditions" folder) 
+%   Folder containing .png  image files               ("stim_images" folder)
+%   Folder containing .bmp  image files               ("stim_images_woreds")
+%   Folder containing .xslx Stimulus Excel Files      ("stim_excel_files" folder) 
 %
 % ---------------------------------------------------------------------------
 % Output(s)
-%   xFile: .mat file to be used in xDiva for task design                            ("xFiles" folder)
-%   
-%
+%   xFile: .mat file to be used in xDiva for task design    ("xFiles" folder)
 
 
-%% Clear Workspace
-tic
-clear
-close all
-clc
-cd ~/Documents/Fang/Magic_Words_Spring_2023/ %Set Working Directory
 
-mainPath = '~/Documents/Fang/Magic_Words_Spring_2023/';
+%% Set Up
+tic %Start Timer 
+
+clear; close all ; clc; % Clear Workspace
+
+% mainPath = ~/Documents/YOUR PATH/create_xFiles_package/
+mainPath = "~/Documents/Lindsey/Deep_Sea_Spellers/create_xFiles_package_AV/";
 xFilesPath = fullfile(mainPath,'xFiles');
+ExcelFolderPath = fullfile(mainPath,'stim_excel_files/');
+imageFolderPath = fullfile(mainPath,'stim_images/');
+wordFolderPath = fullfile(mainPath,'stim_images_words/');
 
-% Create main directory for X-Files
-if (~exist('xFiles', 'dir'))
+if exist('stim_audio','dir') ~= 0
+    audioFilePath = fullfile(mainPath,'stim_audio/');
+
+    soundLength_ms = 500;       % Basing these off of the OneBack_Tracker Excell Sheet
+    soundSamplingRate = 24000;  % Basing these off of the OneBack_Tracker Excell Sheet
+end
+
+% Change Directory to Main Path
+cd(mainPath)
+
+% Create xFiles Folder
+if (~exist('xFile','dir'))
     mkdir(xFilesPath);
 end
-%% Set up user inputs
-classNum = 3; % Number of Class's
 
-conditions = 3; %Range of Conditions
+%% User Inputs
+classNum = 3;
+%conditions = 4; % Number of Sheets in the Excel file, this includes the practice Trial Sheet
+%trials = 10; 
 
-trials = 10; %Number of Trials in each Condition
 
 % Sizing variables (VV)
 objectVerticalSizePix = 80; % vertical size of the words in pixels.
@@ -49,101 +61,183 @@ nCols_full	= 500;
 nRows_small	= 160;
 nCols_small	= 600; % 600;
 
-for classi = 1:classNum
-    for condition = 1:conditions
-        %Change file paths for your computer
-        excelFile = [pwd filesep 'conditions/MW_CLASS' num2str(classi) '.xlsx'];  %Path to excel files
-        emojiFilePath = [pwd filesep 'emoji_Images/']; %Path to Emoji Images
-        wordFilePath = [pwd filesep 'word_Images/']; %Path to Word Images
-    
-        %% Choosing version and initializing variables
-        conditionData = readtable(excelFile, 'Sheet', condition);  %Read in Excel File
-        conditionData = conditionData(:,2:11);
-        %digitImageFile = matfile(digitImageMatFile);
+
+for iClass = 1:classNum
+        % Assign Excel File Path
+        ExcelFilePath = strcat(ExcelFolderPath, 'DSS_Class_', num2str(iClass), '_AV.xlsx'); % Path to excel files
+        sheetNames = sheetnames(ExcelFilePath);
+        sheetNum = length(sheetNames);
         
-        for trial = 1:trials
-            % Convert data to string, split strings, and transpose like above
-            % Added 1 to the trial iterator to account for the stimNum Column
-            stimData = cellstr(transpose(splitlines(string(conditionData.(trial)))));
-            
-            stimImages = [];
-    
-            %% Draft code
-            for imgID = 1:size(stimData, 2)
-                if contains(stimData{:, imgID},'.png') == 1 % .PNG File
-                    emoji_rgb = imread(strcat(emojiFilePath, stimData{:, imgID}));
+       
+        
+    for iSheet = 1:sheetNum
 
-                    %if classi == 3
-                    %    temp = find(emoji_rgb==1);
-                    %    emoji_rgb(temp) = 0;
-                    %end
+         if contains(sheetNames(iSheet), 'practice', 'IgnoreCase',true)
+            conditionCounter = 0; 
+         end
 
-                    emoji_rgb = im2double(emoji_rgb); %convert to double
+         if (~exist('conditionCounter', 'Var'))
+             conditionCoutner = iSheet;
+         end
+        
+        % Read in Condition Data
+        conditionData = readtable(ExcelFilePath, 'Sheet', iSheet); %Read in Condition Table
+        
+        trials = width(conditionData)-1; % Determin and Assign Number of Trials
 
-                    %Convert black background to grey    
-                    greycol = 0.5010;
-                    [rows, cols, panes] = size(emoji_rgb);
-                    blacks = find( ~sum(emoji_rgb,3) );
-                    emoji_rgb( [blacks, blacks + rows*cols, blacks + 2*rows*cols]) = greycol;
-                    %imshow(emoji_rgb)
+        conditionData = conditionData(:,2:(trials+1)); % Remove first column from dataset
+        
 
-                    stimImages(:,:,:, imgID) = emoji_rgb;
-                  
+        if any(contains(conditionData.Properties.VariableNames,'Audio','IgnoreCase',true))
+            trials = trials/2; % Cut Trial numbers in half
+        end
 
-                elseif contains(stimData{:, imgID},'.png') == 0 % TEXT File
-                    %Read in Word Bit Map
-                    wordImg_gray = imread(strcat(wordFilePath, stimData{:, imgID}, '.bmp')); % Read in Gray Scale bit map
-                    [wordImg_Ind, wordImg_Cmap] = gray2ind(wordImg_gray,500); % Convert Gray Scale Image ==> Index Image
-                    word_rgb = ind2rgb(wordImg_Ind, wordImg_Cmap); % Convert Index Image ==> Truecolor RGB Image
+        for iTrial = 1:trials
 
-                    stimImages(:,:,:, imgID) = word_rgb; %Assign to structure
-                    
-                    %{
-                    wordIMG = imread(strcat(wordFilePath, stimData{:, imgID}, '.bmp'));
-                    imshow(wordIMG);
-
-                    [wordIMG_Ind, wordImgCmap] = gray2ind(wordIMG,500);
-
-                    imshow(wordIMG_Ind, wordImgCmap);
-
-                     word_Ind2rgb = ind2rgb(wordIMG_Ind,wordImgCmap);
-
-                    word_Gray2RGB = grs2rgb(wordIMG);
-                    imshow(word_Gray2RGB); 
-                    %}
+            if any(contains(conditionData.Properties.VariableNames,'Audio','IgnoreCase',true))
+                trialIndices = find(contains(conditionData.Properties.VariableNames, num2str(iTrial))); % Find Trial Indices
+                trialSubset = conditionData(:,trialIndices); % Extract Trial Subset
                 
 
+                stimData = cellstr(transpose(splitlines(string(trialSubset.(1))))); %First Column must be Image Files
+                stimImages = []; 
+
+                audioData = transpose(splitlines(strings(trialSubset.(2))));
+                stimAudio = [];
+
+
+                % desired sound length in seconds; if input sound is shorter, silence will
+                % be added to the end, and if input sound is longer, it will be cut from the end to
+                % this length
+                desiredSoundLength = soundLength_ms / 1000;
+                soundRateHz = soundSamplingRate;
+
+                desiredNumberOfSamples = desiredSoundLength * soundRateHz;
+
+            else
+                % Convert data to string, split strings at New-Line Characters, and transpose
+                stimData = cellstr(transpose(splitlines(string(conditionData.(iTrial)))));
+                stimImages = [];
+            end
+            
+
+            for imgID = 1:size(stimData, 2)
+                if contains(stimData{:, imgID}, '.png') == 0 % .bmp File
+                    %%% Read in Word Bit Map %%%
+
+
+                    % Read in Gray Scale Bit Map
+                    wordImg_gray = imread(strcat(wordFolderPath, stimData{:, imgID}, '.bmp')); 
+
+                    %Convert Gray Scale Image to Index Image
+                    [wordImg_Ind, wordImg_Cmap] = gray2ind(wordImg_gray,500); 
+
+                    % Converty INdex Image to Truecolor RGB Image
+                    word_rgb = ind2rgb(wordImg_Ind, wordImg_Cmap); 
+
                     
-                    %Genereate Text .mat File
-                    %image = makeTextImages(stimData{:, imgID}, nRows_small, nCols_small);  
+
+                    % Assign to Stim Image Structure
+                    stimImages(:,:,:, imgID) = word_rgb;
+
+
+                elseif contains(stimData{:, imgID}, '.png') == 1 % .png File
+                    %%% Read in .png Image %%%
+                    img_rgb = imread(strcat(imageFolderPath, stimData{:, imgID}));
+
+                    img_rgb = im2double(img_rgb); % convert to double
+
+                    % Convert Black background to grey
+                    greycol = 0.5010;
+                    [rows, cols, panes] = size(img_rgb);
+                    black_values = find( ~sum(img_rgb,3));
+                    img_rgb([black_values, black_values + rows*cols, black_values + 2*rows*cols]) = greycol;
+                    %imshow(img_rgb) 
+
+                    stimImages(:,:,:, imgID) = img_rgb;
+
+
                 end
             end
-                
-    
-            %% Saving the output files
-            images = single(stimImages);
-            strings = stimData;
-            %objectVerticalSizePix = ;
            
-            % Create sub-directories for X-Files
-            classFolder = strcat(xFilesPath, '/Class_', (num2str(classi)));
+            %% Stim Pair Generation
+            if any(contains(conditionData.Properties.VariableNames,'Audio','IgnoreCase',true))
+                for tokenID = 1:length(audioData)
+                    currentAudio = soundsInput(tokenID);
+                    currentAudioData = audioread(audioFilePath + currentAudio);
+                    %currentCongruentString = congruentDots(tokenID);
+                    %currentIncongruentString = incongruentDots(tokenID);
+                    currentNumberOfSamples = length(currentAudioData);
+                    if currentNumberOfSamples > desiredNumberOfSamples
+                        currentAudioData = (currentAudioData(1:desiredNumberOfSamples, :));
+                    elseif currentNumberOfSamples < desiredNumberOfSamples
+                        currentAudioData = [currentAudioData; zeros(desiredNumberOfSamples - currentNumberOfSamples, 1)];
+                    end
+                    stimAudio = [stimAudio, currentAudioData];
+                end
+            end
+
+            %% Create sub-directories for xFiles
+            
+            % Create Class Sub-Folder
+            classFolder = strcat(xFilesPath, '/Class_', (num2str(iClass)));
             if (~exist((classFolder),'dir'))
                 mkdir(classFolder);
             end
+            
+            if contains(sheetNames(iSheet), 'practice', 'IgnoreCase',true)
+                % Create Practice Sub-Folder
+                practiceFolder = strcat(classFolder, '/Class_', num2str(iClass), '_Practice');
+                if (~exist((practiceFolder), 'dir'))
+                    mkdir(practiceFolder);
+                end
 
-            conditionFolder = strcat(classFolder, '/Class_', num2str(classi), '_Condition_', num2str(condition)+1);
-            if (~exist((conditionFolder),'dir'))
-                mkdir(conditionFolder);
+            elseif ~(contains(sheetNames(iSheet), 'practice', 'IgnoreCase',true))
+                % Create Condition Sub-Folder
+                conditionFolder = strcat(classFolder, '/Class_', num2str(iClass), '_Condition_', num2str(conditionCounter));
+                if (~exist((conditionFolder),'dir'))
+                    mkdir(conditionFolder);
+                end
             end
 
-            save([pwd filesep 'xFiles/Class_'  num2str(classi) ...
-                '/Class_' num2str(classi) '_Condition_' (num2str(condition)+1) ...
-                '/MW_Class_' num2str(classi) '_condition_' (num2str(condition)+1) '_Trial_' num2str(trial) '.mat'], ...
-                'images', 'strings'); %, 'objectVerticalSizePix');
-         
+            %% Save Out
+            images = single(stimImages);
+            strings = stimData; 
+            %objectVerticalSizePix = ;
             
+            if contains(sheetNames(iSheet), 'practice', 'IgnoreCase',true)    % If there are Practice Trials
+                if ~exist('stimAudio', 'var') && ~exist('soundRateHz', 'var') % If there is NO Audio Data
+                    save([xFilesPath filesep 'xFiles/Class_' num2str(iClass) ...                       % Class Folder 
+                        '/Class_' num2str(iClass) '_Practice/' ...                                     % Practice Folder
+                        '/DSS_Class_' num2str(iClass) '_Practice_Trial_' num2str(iTrial) '.mat'], ...  % File Name
+                        'images', 'strings'); %, 'objectVerticalSizePix');                             % Output Variables
+
+                elseif exist('stimAudio', 'var') && exist('soundRateHz', 'var') % If Audio Data Exists
+                    save([xFilesPath filesep 'xFiles/Class_' num2str(iClass) ...                       % Class Folder 
+                        '/Class_' num2str(iClass) '_Practice/' ...                                     % Practice Folder
+                        '/DSS_Class_' num2str(iClass) '_Practice_Trial_' num2str(iTrial) '.mat'], ...  % File Name
+                        'images', 'strings', 'stimAudio', 'soundRateHz'); %, 'objectVerticalSizePix'); % Output Variables
+                end
+
+
+            elseif ~(contains(sheetNames(iSheet), 'practice', 'IgnoreCase',true)) % If there are NO Practice Trials
+                if  ~exist('stimAudio', 'var') && ~exist('soundRateHz', 'var')    % If there is NO Audio Data
+                    save([xFilesPath filesep 'Class_' num2str(iClass) ...                                                            % Class Folder
+                        '/Class_' num2str(iClass) '_Condition_' num2str(conditionCoutner) ...                                        % Condition Folder
+                        '/DSS_Class_' num2str(iClass) '_condition_' num2str(conditionCoutner) '_Trial_' num2str(iTrial) '.mat'], ... % File Name
+                        'images', 'strings'); %, 'objectVerticalSizePix');                                                           % Output Variables
+                    
+                elseif exist('stimAudio', 'var') && exist('soundRateHz', 'var') % If Audio Data Exists
+                    save([xFilesPath filesep 'Class_' num2str(iClass) ...                                                            % Class Folder
+                        '/Class_' num2str(iClass) '_Condition_' num2str(conditionCoutner) ...                                        % Condition Folder
+                        '/DSS_Class_' num2str(iClass) '_condition_' num2str(conditionCoutner) '_Trial_' num2str(iTrial) '.mat'], ... % File Name
+                        'images', 'strings','stimAudio', 'soundRateHz'); %, 'objectVerticalSizePix');                                % Output Variables
+                end
+            end
+
         end
+        conditionCounter = conditionCounter + 1;
     end
 end
 
-toc
+toc % End Timer
